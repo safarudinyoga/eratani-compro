20897387d4678c6197e15cc140040df922491a6f
safarudinyoga <safarudinyoga@gmail.com>
2022-06-23 04:47:19 +0700

    :zap: career responsive

--- hooks/useWindowDimensions.js
+++ hooks/useWindowDimensions.js
@@ -2,20 +2,22 @@ import { useState, useEffect } from 'react';
 
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState({
-    width: 0,
-    height: 0
+    width: undefined,
+    height: undefined
   });
 
   useEffect(() => {
-    function handleResize() {
-      setWindowDimensions({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-    }
+    if (typeof window !== "undefined") {
+      function handleResize() {
+        setWindowDimensions({
+          width: window.innerWidth,
+          height: window.innerHeight
+        });
+      }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }
   }, []);
 
   return windowDimensions;
